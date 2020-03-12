bc954ad42452c6d6b7d927a5f125c030d600bbaf
Nur  Ihsan <ihsan@fas.id>
2020-03-12 13:21:49 +0700

    fix notif

--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,103 @@
-import * as React from 'react';
-import {Text, View, TouchableOpacity, Platform} from 'react-native';
-import PushNotification from 'react-native-push-notification';
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import firebase from 'react-native-firebase';
+
 export const navigationRef = React.createRef();
 
 function navigate(name, params) {
   navigationRef.current && navigationRef.current.navigate(name, params);
 }
 
-export function setupPushNotification(handleNotification) {
-  PushNotification.configure({
-    onNotification: function(notification) {
-      handleNotification(notification);
-      if (Platform.OS === 'ios') {
-        notification.finish(PushNotificationIOS.FetchResult.NoData);
+export default class Home extends Component {
+  async componentDidMount() {
+    const enabled = await firebase.messaging().hasPermission();
+
+    if (enabled) {
+      const fcmToken = await firebase.messaging().getToken();
+      console.log('fcmToken', fcmToken);
+
+      this.notificationListener = firebase
+        .notifications()
+        .onNotification(notification => {
+          let notificationMessage =
+            notification._android._notification._data.action;
+          let recordId = notification._android._notification._data.recordID;
+
+          let {title, body} = notification;
+          console.log(title, body, notificationMessage, recordId);
+
+          const channelId = new firebase.notifications.Android.Channel(
+            'Default',
+            'Default',
+            firebase.notifications.Android.Importance.High,
+          );
+          firebase.notifications().android.createChannel(channelId);
+
+          let notification_to_be_displayed = new firebase.notifications.Notification(
+            {
+              data: notification._android._notification._data,
+              sound: 'default',
+              show_in_foreground: true,
+              title: notification.title,
+              body: notification.body,
+            },
+          );
+
+          if (Platform.OS == 'android') {
+            notification_to_be_displayed.android
+              .setPriority(firebase.notifications.Android.Priority.High)
+              .android.setChannelId('Default')
+              .android.setVibrate(1000)
+              .android.setSmallIcon('ic_stat_ic_notification');
+          }
+          console.log(
+            'FOREGROUND NOTIFICATION LISTENER: \n',
+            notification_to_be_displayed,
+          );
+
+          firebase
+            .notifications()
+            .displayNotification(notification_to_be_displayed);
+        });
+
+      /*
+       * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+       * */
+      this.notificationOpenedListener = firebase
+        .notifications()
+        .onNotificationOpened(notificationOpen => {
+          const {screen} = notificationOpen.notification._data;
+          console.log(navigate(screen));
+          firebase
+            .notifications()
+            .removeDeliveredNotification(
+              notificationOpen.notification.notificationId,
+            );
+        });
+
+      /*
+       * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+       * */
+      const notificationOpen = await firebase
+        .notifications()
+        .getInitialNotification();
+      if (notificationOpen) {
+        const {screen} = notificationOpen.notification._data;
+        console.log(navigate(screen));
       }
-    },
-    popInitialNotification: true,
-    requestPermissions: true,
-  });
-
-  return PushNotification;
-}
-
-export default class Home extends React.Component {
-  componentDidMount() {
-    this.pushNotification = setupPushNotification(this._handleNotificationOpen);
-  }
-
-  _handleNotificationOpen = notification => {
-    // alert(notification.screen);
-    console.log(notification);
-    if (notification.userInteraction) {
-      navigate(notification.screen);
+    } else {
+      try {
+        firebase.messaging().requestPermission();
+      } catch (e) {
+        alert('user rejected the permissions');
+      }
     }
-  };
+  }
 
   render() {
     return (
-      <View>
-        <TouchableOpacity onPress={() => navigate('Detail')}>
-          <Text> Home </Text>
-        </TouchableOpacity>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> Push Notif </Text>
       </View>
     );
   }
